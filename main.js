74333fd281e4acadcf64e18cf1ce97663a577e9e
leonardopbl <leonardo.pbl@hotmail.com>
2022-11-04 21:49:29 -0300

    :tada: commit realizado na aula 5

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ function createGame(player1, hour, player2) {
     </li>
   `
 }
+
+let delay = -0.4;
 function createCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-    <div class="card">
+    <div class="card style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
       <ul>
         ${games}
@@ -18,18 +21,12 @@ function createCard(date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW" />
-  </header>
-  <main id="cards">
-    ${createCard("24/11", "quinta", createGame("Brazil", "16:00", "Serbia"))}
-    ${createCard(
-      "28/11",
-      "Segunda",
-      createGame("Brazil", "13:00", "Switzerland") +
-        createGame("Portugal", "16:00", "Uruguay")
-    )} 
-    ${createCard("02/12", "Segunda", createGame("Cameron", "16:00", "Brazil"))} 
-</main>
-`
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("Brazil", "16:00", "Serbia")) +
+  createCard(
+    "28/11",
+    "Segunda",
+    createGame("Brazil", "13:00", "Switzerland") +
+      createGame("Portugal", "16:00", "Uruguay")
+  ) +
+  createCard("02/12", "Segunda", createGame("Cameron", "16:00", "Brazil"))
